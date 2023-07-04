27c776bbbd8d4cc4c8597b4fe84ff4210fd3bea5
Neelesh Bohre <109515400+Neelesh1983@users.noreply.github.com>
2023-07-04 17:12:29 +0530

    Update MyfirstApp.py

--- MyfirstApp.py
+++ MyfirstApp.py
@@ -1,2 +1,9 @@
 import streamlit as st
 import snowflake.connector as sncon
+
+cnt1 = st.container()
+cnt.header("Dashboard")
+
+my_cnx = stc.connect(**st.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+mycur.execute("select state, country from COVID19_EPIDEMIOLOGICAL_DATA.PUBLIC.DEMOGRAPHICS")
