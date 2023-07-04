f30285e972bde9192b8d82b9c45149d9a9fe79e7
Neelesh Bohre <109515400+Neelesh1983@users.noreply.github.com>
2023-07-04 18:20:09 +0530

    Update MyfirstApp.py

--- MyfirstApp.py
+++ MyfirstApp.py
@@ -1,12 +1,13 @@
 import streamlit as st
 import snowflake.connector as sncon
 
-cnt1 = st.container()
-cnt.header("Dashboard")
-
 my_cnx = sncon.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 mycur.execute("select top 100 state, county from DEMOGRAPHICS")
 dr = mycur.fetchall()
 df = st.dataframe(dr)
+
+
+cnt1 = st.container()
+cnt.header("Dashboard")
 df
