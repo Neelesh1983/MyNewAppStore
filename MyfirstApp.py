817a5f76768fd33a71685189c90a03a845e78e10
Neelesh Bohre <109515400+Neelesh1983@users.noreply.github.com>
2023-07-05 13:33:35 +0530

    Update MyfirstApp.py

--- MyfirstApp.py
+++ MyfirstApp.py
@@ -1,11 +1,4 @@
 import streamlit as st
-import snowflake.connector as sncon
+#import snowflake.connector as sncon
 
-my_cnx = sncon.connect(
-    user="Neelesh2023",
-    password="Neelesh@2023",
-    account="XN50895.ap-southeast-1.aws",
-    database = "COVID19_EPIDEMIOLOGICAL_DATA",
-     schema = "PUBLIC"
-    )
-my_cnx.cursor().execute("select top 100 state, county from DEMOGRAPHICS")
+st.write(**st.secrets["snowflake"])
